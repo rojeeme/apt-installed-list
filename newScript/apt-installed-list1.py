2a36ee73453c010b21b0dd6477831394a4a17007
IT19112824_Rojeemeeharan E <it19112824@my.sliit.lk>
2022-12-16 13:35:31 +0530

    table formate updated

--- newScript/apt-installed-list1.py
+++ newScript/apt-installed-list1.py
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Store the details of all installed packages in a file called "installed-packages.txt"
-dpkg -l | awk '{print $2, $3, $4, $5}' > installed-packages.txt
+#  dpkg -l | awk '{print $2, $3, $4, $5}' > installed-packages.txt
+
+dpkg -l | column -s $'\t' -t -c 20 > installed-packages.txt
